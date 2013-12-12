35367dfb009f041b34d22d3759296d2eb7e2894d
mathieu <mat.simonnet@gmail.com>
2013-12-12 22:33:02 +0100

    clean up

--- src/sm/tb/geotabproj/GeoTabMapDatabaseCallback.java
+++ src/sm/tb/geotabproj/GeoTabMapDatabaseCallback.java
@@ -26,7 +26,7 @@ public class GeoTabMapDatabaseCallback implements MapDatabaseCallback{
 		for (int i = 0; i < tags.size(); i++)	{
 			poi.addTag(tags.get(i).key, tags.get(i).value);
 //			Log.i("renderPointOfInterest", "key = " + tags.get(i).key.toString() + " // value = " + tags.get(i).value.toString());
-			if (tags.get(i).key.equals("place") && tags.get(i).value.equals("city")){ //&& tags.get(i).key.equals("station") 
+			if (tags.get(i).key.equals("place") /*&& tags.get(i).value.equals("city")*/){ //&& tags.get(i).key.equals("station") 
 				pois.add(poi);
 //				for (int iP = 0; iP < pois.size(); iP++){
 //					Log.i("pois" + i , pois.get(iP).getTags().toString() + "" );
